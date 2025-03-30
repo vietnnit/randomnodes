7404cb4199d6744d3cf81b1d60f499be6a8a524a
NGUYEN NGOC VIET <vietnnit@gmail.com>
2025-03-30 14:10:45 +0700

    Update ritual-node.sh

--- ritual-node.sh
+++ ritual-node.sh
@@ -51,7 +51,36 @@ install_ritual() {
     private_key="0x$private_key"
     echo "Added 0x prefix to private key"
   fi
-
+  
+  echo "Installing dependencies..."
+  
+  # Update packages & build tools
+  sudo apt update && sudo apt upgrade -y
+  sudo apt -qy install curl git jq lz4 build-essential screen
+  
+  # Install Docker
+  echo "Installing Docker..."
+  sudo apt-get update
+  sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  sudo docker run hello-world
+  
+  # Install Docker Compose
+  echo "Installing Docker Compose..."
+  sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+  mkdir -p $DOCKER_CONFIG/cli-plugins
+  curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+  docker compose version
+  sudo usermod -aG docker $USER
+  docker run hello-world
   
   # Clone Repository
   echo "Cloning repository..."
